674bcfe360885419c4e75f1ac542ea717e2fb926
Pavel Zubko <tyyrok@gmail.com>
2023-09-20 14:47:44 +0400

    learning_app: add models

--- learning_app/admin.py
+++ learning_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProducts, Product, UserLessonInfo, Lesson
 
 # Register your models here.
+admin.site.register(UserLessonInfo)
+admin.site.register(Product)
+admin.site.register(Lesson)
+admin.site.register(UserProducts)
--- learning_app/models.py
+++ learning_app/models.py
@@ -1,1 +1,48 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from datetime import datetime
+
+User = get_user_model()
+
+class Product(models.Model):
+    title = models.CharField(max_length=128)
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return f"{self.title} by {self.owner}"
+    
+class UserProducts(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return f"pk-{self.pk}, User-{self.user.username}"
+
+class Lesson(models.Model):
+    product = models.ManyToManyField(to=Product, blank=True)
+    title = models.CharField(max_length=128)
+    video_link = models.URLField()
+    duration = models.DurationField()
+    
+    def __str__(self) -> str:
+        return f"{self.title} - {self.video_link} - {self.duration}"
+
+class UserLessonInfo(models.Model):
+    STATUS = [
+        ('YES', 'Watched'),
+        ('NO', 'Not watched'),
+    ]
+    watched_time = models.DurationField(default=0)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(to=Lesson, on_delete=models.CASCADE)
+    
+    @property
+    def status(self):
+        if self.watched_time >= self.lesson.duration * 0.8:
+            return self.STATUS[0][1]
+        else:
+            return self.STATUS[1][1]
+        
+    def __str__(self) -> str:
+        return f"Lesson - {self.lesson.title}, User - {self.user.username}, Status - {self.status}"
+        
